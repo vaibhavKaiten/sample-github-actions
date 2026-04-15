3f7708e1d6745ab755b0927d3f3fe66195242ae8
Vaibhav Gupta <vaibhav@kaitensoftware.com>
2026-04-15 17:08:50 +0530

    rm softwares

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! , From kaiten ');
+    res.send('Hello World! , From Jaipur Rajasthan');
 });
 
 
